ffde6831ebb1af905c038132d05d11039588b53d
Szumilas <gszumilas92@gmail.com>
2017-10-19 11:27:33 +0200

    Learning BodyParser

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 let port = process.env.PORT || 3000;
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+let jsonParser = bodyParser.json();
 
 //Using Middleware
 app.use('/assets', express.static(__dirname + '/public'));
@@ -12,12 +15,25 @@ app.use('/', function(req, res, next) {
     next();
 });
 
+//BodyParser Form
 app.get('/', function(req, res) {
-    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet></head><body><h1>Hello World!</h1></body></html>');
+    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet></head><body><h1>Hello World!</h1><form method="POST" action="/person">First name: <input type="text" id = "firstname" name="firstname"/></form></body></html>');
 });
 
 app.get('/person/:id', function(req, res) {
     res.send(`<html><head></head><body><h1>Person: ${req.params.id}</h1></body></html>`);
+});
+
+//BodyParser Json Usage
+app.post('personjson', jsonParser, function(req,res) {
+    res.send('Thank you for json data');
+    console.log(req.body.firstname);
+});
+
+//object body added to request by 'body-parser'
+app.post('/person', urlencodedParser, function(req, res) {
+    res.send('Thank you!');
+    console.log(req.body.firstname);
 });
 
 app.get('/api', function(req, res) {
