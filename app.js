74a65f4c31c5d16ecaf4765f3e94a35da736534a
Szumilas <gszumilas92@gmail.com>
2017-10-19 10:35:56 +0200

    Using / Creating Middleware

--- app.js
+++ app.js
@@ -3,19 +3,28 @@ const app = express();
 
 let port = process.env.PORT || 3000;
 
+//Using Middleware
+app.use('/assets', express.static(__dirname + '/public'));
+
+//Creating my own Middleware
+app.use('/', function(req, res, next) {
+    console.log(`Request Url ${req.url}`);
+    next();
+});
+
 app.get('/', function(req, res) {
-    res.send('<html><head></head><body><h1>Hello World!</h1></body></html>');
-})
+    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet></head><body><h1>Hello World!</h1></body></html>');
+});
 
 app.get('/person/:id', function(req, res) {
     res.send(`<html><head></head><body><h1>Person: ${req.params.id}</h1></body></html>`);
-})
+});
 
 app.get('/api', function(req, res) {
     res.json({ 
         firstname: 'Grzegorz',
         lastname: 'Szumilas'
     });
-})
+});
 
 app.listen(port);
